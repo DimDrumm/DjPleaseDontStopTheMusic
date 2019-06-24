54427bc4e7a9af7722d4c4ff1a5ea778061590bb
ITSTEP\st <stepjava2@gmail.com>
2019-06-24 21:46:14 +0300

    +ReadFrom assets +method play in SongFragment

--- app/src/main/java/a2018/by/step/musicplayer/MainActivity.kt
+++ app/src/main/java/a2018/by/step/musicplayer/MainActivity.kt
@@ -2,17 +2,23 @@ package a2018.by.step.musicplayer
 
 import a2018.by.step.musicplayer.fragment.SongFragment
 import a2018.by.step.musicplayer.fragment.SongListFragment
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
+
+const val CODE_REQUEST_READ_EXTERNAL_STORAGE = 501
 
 class MainActivity : AppCompatActivity(), SongListFragment.ISongItemClickListener {
     override fun onSongClicked(item: Int) {
         Timber.d("Activity knows that item $item have been clicked")
         fl_container_song.visibility = View.VISIBLE
-        fl_container_song.scaleY=0.0f
+        fl_container_song.scaleY = 0.0f
         supportFragmentManager.beginTransaction().replace(
             R.id.fl_container_song,
             SongFragment.newInstance(item)
@@ -23,8 +29,46 @@ class MainActivity : AppCompatActivity(), SongListFragment.ISongItemClickListene
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.fl_container_song_list, SongListFragment.newInstance())
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            continueWithPermission()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), CODE_REQUEST_READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CODE_REQUEST_READ_EXTERNAL_STORAGE -> {
+                if (permissions[0] == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        continueWithPermission()
+                    } else {
+                        continueWithoutPermission()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun continueWithPermission() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_container_song_list, SongListFragment.newInstance())
             .commit()
-        Timber.d("FragmentManager backStack: ${supportFragmentManager.backStackEntryCount}")
+    }
+
+    private fun continueWithoutPermission() {
+        Timber.d("User has denied permission ${Manifest.permission.READ_EXTERNAL_STORAGE}")
+        Toast.makeText(this,"You are so rude, maaaan!",Toast.LENGTH_SHORT).show()
+        finish()
     }
 }
